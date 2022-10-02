9fa8cb432b2ff299173d1035ce88cfc05ee6fc3e
Johnny <Johnnyho9293@gmail.com>
2022-10-01 17:28:25 -0700

    Firebase setup

--- my-app/src/components/Messages/Messages.jsx
+++ my-app/src/components/Messages/Messages.jsx
@@ -1,5 +1,39 @@
+import React from 'react'
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import 'firebase/compat/analytics';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDtmqZDJSPUCSDkAMNPw3fJsv2vLI7vt7Q",
+  authDomain: "reading-rainforest.firebaseapp.com",
+  projectId: "reading-rainforest",
+  storageBucket: "reading-rainforest.appspot.com",
+  messagingSenderId: "15291258330",
+  appId: "1:15291258330:web:a46010f99e54fb33d8dce3",
+  measurementId: "G-MT7Q718G7Q"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 const Messages = () => {
-  return <h1>Messages</h1>;
+  const [user] = useAuthState(auth)
+
+  return (
+    <div>
+      <header>
+
+      </header>
+      <section>
+        {/* {user ? <ChatRoom /> : <SignIn />} */}
+      </section>
+    </div>
+  );
 };
 
 export default Messages;
