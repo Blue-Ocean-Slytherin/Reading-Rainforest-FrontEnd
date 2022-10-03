0cc5b632bde8cc60b63f372ad1dd8d427c3381a5
dGarcia-NC <anthonygarcia833@gmail.com>
2022-10-03 10:08:32 -0700

    Completed AddToListing Modal

--- my-app/src/components/ListToProfile/SearchBar.jsx
+++ my-app/src/components/ListToProfile/SearchBar.jsx
@@ -1,10 +1,11 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Paper from "@mui/material/Paper";
 import InputBase from "@mui/material/InputBase";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import axios from "axios";
 
 const theme = createTheme({
   palette: {
@@ -26,7 +27,19 @@ const theme = createTheme({
   },
 });
 
-export default function SearchBar() {
+export default function SearchBar({ Books }) {
+  const [search, setSearch] = useState("");
+
+  const handleClick = () => {
+    if (search.length > 0) {
+      axios
+        .get(`https://www.googleapis.com/books/v1/volumes?q=${search}`)
+        .then((response) => {
+          Books(response.data.items);
+        });
+    }
+  };
+
   return (
     <Paper
       component="form"
@@ -37,11 +50,13 @@ export default function SearchBar() {
         width: 400,
         m: "auto",
       }}
+      onSubmit={(e) => e.preventDefault()}
     >
       <InputBase
         sx={{ ml: 1, flex: 1 }}
         placeholder="Search Query (Name, ISBN, Author, Etc)"
-        inputProps={{ "aria-label": "search google maps" }}
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
       <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
       <ThemeProvider theme={theme}>
@@ -49,6 +64,7 @@ export default function SearchBar() {
           color="spanishGreen"
           sx={{ p: "10px" }}
           aria-label="directions"
+          onClick={handleClick}
         >
           <SearchIcon />
         </IconButton>
