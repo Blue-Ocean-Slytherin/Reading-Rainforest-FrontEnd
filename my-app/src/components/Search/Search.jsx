c873761885cb98342576903f53f447e0c9ff0eec
dGarcia-NC <anthonygarcia833@gmail.com>
2022-10-04 16:30:33 -0700

    Initial render for search page

--- my-app/src/components/Search/Search.jsx
+++ my-app/src/components/Search/Search.jsx
@@ -7,7 +7,7 @@ import { useLocation } from "react-router-dom";
 import axios from "axios";
 
 const Search = () => {
-  const [foundBooks, setFoundBooks] = React.useState([]);
+  const [booksData, setBooksData] = React.useState({});
   const location = useLocation();
   const { searchInput } = location.state;
   console.log("The search input:", searchInput);
@@ -15,14 +15,16 @@ const Search = () => {
     if (searchInput.length !== "") {
       console.log("Search Input:", searchInput);
       axios
-        .get(`http://localhost:3001/books/${searchInput}`)
+        .get(`http://localhost:3002/search/books/${searchInput}`)
         .then((response) => {
-          console.log("response:", response);
+          console.log("response:", response.data);
+          setBooksData(response.data);
+          console.log("Books data:", booksData);
         })
-
         .catch((error) => console.log(error));
     }
-  });
+  }, [searchInput]);
+
   return (
     <React.Fragment>
       <CssBaseline />
@@ -32,11 +34,18 @@ const Search = () => {
           spacing={{ xs: 2, md: 3 }}
           columns={{ xs: 4, sm: 8, md: 12 }}
         >
-          {Array.from(Array(9)).map((_, index) => (
-            <Grid item xs={2} sm={4} md={4} key={index}>
-              <BookCard />
-            </Grid>
-          ))}
+          {booksData.userData ? (
+            booksData.userData.map((data, index) => {
+              console.log("books data:", booksData.userData);
+              return (
+                <Grid item xs={2} sm={4} md={4} key={index}>
+                  <BookCard user={data} book={booksData.bookData} />
+                </Grid>
+              );
+            })
+          ) : (
+            <p>No Books Found</p>
+          )}
         </Grid>
       </Container>
     </React.Fragment>
