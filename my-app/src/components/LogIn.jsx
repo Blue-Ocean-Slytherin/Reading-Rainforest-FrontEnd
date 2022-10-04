428acdfecd7524febc6c6fac8764276330f7ad32
Sean Young <SeanKaicheeYoung@gmail.com>
2022-10-04 15:49:29 -0700

    Working on Registration Page

--- my-app/src/components/LogIn.jsx
+++ my-app/src/components/LogIn.jsx
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
-import React, { useEffect } from 'react';
-import jwt_decode from 'jwt-decode';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth, firestore, firebase } from '../firebase'
+import React, { useState } from 'react';
+import { firebase } from '../firebase'
 import img from "../images/LogInImage.webp";
 import logo from "../images/ReadingRainforestLogo.png"
 
-import SwapHorizIcon from "@mui/icons-material/SwapHoriz";
+import TextField from '@mui/material/TextField';
 import Button from "@mui/material/Button";
+import GoogleIcon from '@mui/icons-material/Google';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 const LogInContainer = styled.div`
 display: flex;
@@ -34,6 +34,10 @@ const DisplayBox = styled.div`
   padding: 10px 0 10px 0;
 `;
 
+const RegisterBox = styled(DisplayBox)`
+  height: 550px;
+`;
+
 const Welcome = styled.div`
   font-size: 30px;
   text-align: center;
@@ -50,6 +54,10 @@ background-size: contain no-repeat;
 background-position: center;
 `;
 
+const RegiLogo = styled(Logo)`
+height:150px;
+`
+
 const Container = styled.div`
   display: flex;
   justify-content: center;
@@ -57,11 +65,23 @@ const Container = styled.div`
   width: 70%;
 `;
 
+const inputStyle = {
+  padding: '3px',
+  width: '350px',
+}
+
+const RegButtonStyle = {
+  marginTop: '17px',
+  marginBottom: '17px',
+};
+
 export let firebase_auth = firebase.auth();
 
 const LogIn = ({ setUser }) => {
 
-  let handleAuthStateChanged = (user) => {
+  const [ isNewUser, setIsNewUser ] = useState(false);
+
+  let handleAuthStateChanged = async (user) => {
     if (user) {
       // temporary
       let temp = {
@@ -70,10 +90,17 @@ const LogIn = ({ setUser }) => {
         profilePhoto: user._delegate.photoURL,
         phoneNumber: user._delegate.phoneNumber,
         uid: user._delegate.uid,
+        lat: "0",
+        long: "0",
       }
       setUser(temp);
 
       // use the user._delegate.uid to query our DB for user data
+      // let userData = await axios.get("BE URI")
+
+      // temporary for dev, set state manually here
+      // setIsNewUser(true); // is new user
+
       // old user
       // set returned data in setUser function
 
@@ -90,15 +117,30 @@ const LogIn = ({ setUser }) => {
 
   return (
     <LogInContainer>
-      <DisplayBox>
-        <div></div>
-        <Container>
-          <Welcome>Welcome To</Welcome>
-        </Container>
-        <Logo></Logo>
-        <Button variant="contained" color="mintGreen" onClick={handleLogIn} endIcon={<SwapHorizIcon />} > >Sign In With Google</Button>
-        <div></div>
-      </DisplayBox>
+      { isNewUser ?
+          <RegisterBox>
+          <div></div>
+          <Container>
+            <Welcome>Registeration</Welcome>
+          </Container>
+          <RegiLogo></RegiLogo>
+          <TextField variant='filled' label='Full Name' sx={inputStyle}></TextField>
+          <TextField variant='filled' label='Email' sx={inputStyle}></TextField>
+          <TextField variant='filled' label='Phone Number' sx={inputStyle}></TextField>
+          <TextField variant='filled' label='Location' sx={inputStyle}></TextField>
+          <Button variant="contained" color="mintGreen" endIcon={<ArrowForwardIosIcon/>} sx={RegButtonStyle}>Finish Registeration</Button>
+        </RegisterBox>
+        :
+        <DisplayBox>
+          <div></div>
+          <Container>
+            <Welcome>Welcome To</Welcome>
+          </Container>
+          <Logo></Logo>
+          <Button variant="contained" color="mintGreen" onClick={handleLogIn} endIcon={<GoogleIcon />} > >Sign In With Google</Button>
+          <div></div>
+        </DisplayBox>
+      }
     </LogInContainer>
   )
 
