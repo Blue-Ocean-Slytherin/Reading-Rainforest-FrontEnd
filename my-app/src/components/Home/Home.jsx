6fdf7dc7533222c05b190855a09c4f80b5c2611a
Aaron Yabut <aaronjiego@yahoo.com>
2022-10-04 22:18:12 -0700

    dummy data moved

--- my-app/src/components/Home/Home.jsx
+++ my-app/src/components/Home/Home.jsx
@@ -1,138 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Carousel from 'react-material-ui-carousel';
 import Item from './Item.jsx';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
+import { items } from './HomeTempData.jsx'
+import axios from 'axios';
+const API_URL = 'http://localhost:3001'
 
 
 const Home = () => {
   const {booksNear, setBooksNear} = useState([])
   const {topRated, setTopRated} = useState([])
 
-  var items = [
-    {
-      profilePic: "https://images.ctfassets.net/usf1vwtuqyxm/6yl7KHNCnoctW5TGrOyNE1/d81e73479f15d2dfeb37dcf56c47b6b0/HP-F6-half-blood-prince-draco-malfoy-looking-concerned-web-landscape?fm=jpg&q=70&w=2560",
-      bookTitle: "Harry Potter and the Prince of Slytherin by The Sinister Man",
-      date: "December 1, 2019",
-      bookCover: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1609896187i/31201472.jpg",
-      bookDescription: "Harry Potter was Sorted into Slytherin after a crappy childhood. His brother Jim is believed to be the BWL. Think you know this story? Think again. Year Four starts on 9/1/20. ",
-      username: "User 1",
-      distance: 1,
-      rating: 4,
-    },
-    {
-      profilePic: "https://images.ctfassets.net/usf1vwtuqyxm/6yl7KHNCnoctW5TGrOyNE1/d81e73479f15d2dfeb37dcf56c47b6b0/HP-F6-half-blood-prince-draco-malfoy-looking-concerned-web-landscape?fm=jpg&q=70&w=2560",
-      bookTitle: "Harry Potter and the Prince of Slytherin by The Sinister Man",
-      date: "December 1, 2019",
-      bookCover: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1609896187i/31201472.jpg",
-      bookDescription: "Harry Potter was Sorted into Slytherin after a crappy childhood. His brother Jim is believed to be the BWL. Think you know this story? Think again. Year Four starts on 9/1/20. NO romantic pairings prior to Fourth Year. Basically good Dumbledore and Weasleys. Limited bashing (mainly of James).",
-      username: "User 2",
-      distance: 2,
-      rating: 2,
-    },
-    {
-      profilePic: "https://images.ctfassets.net/usf1vwtuqyxm/6yl7KHNCnoctW5TGrOyNE1/d81e73479f15d2dfeb37dcf56c47b6b0/HP-F6-half-blood-prince-draco-malfoy-looking-concerned-web-landscape?fm=jpg&q=70&w=2560",
-      bookTitle: "Harry Potter and the Prince of Slytherin by The Sinister Man",
-      date: "December 1, 2019",
-      bookCover: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1609896187i/31201472.jpg",
-      bookDescription: "Harry Potter was Sorted into Slytherin after a crappy childhood. His brother Jim is believed to be the BWL. Think you know this story? Think again. Year Four starts on 9/1/20. NO romantic pairings prior to Fourth Year. Basically good Dumbledore and Weasleys. Limited bashing (mainly of James).",
-      username: "User 3",
-      distance: 1,
-      rating: 2.5,
-    },
-    {
-      profilePic: "https://images.ctfassets.net/usf1vwtuqyxm/6yl7KHNCnoctW5TGrOyNE1/d81e73479f15d2dfeb37dcf56c47b6b0/HP-F6-half-blood-prince-draco-malfoy-looking-concerned-web-landscape?fm=jpg&q=70&w=2560",
-      bookTitle: "Harry Potter and the Prince of Slytherin by The Sinister Man",
-      date: "December 1, 2019",
-      bookCover: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1609896187i/31201472.jpg",
-      bookDescription: "Harry Potter was Sorted into Slytherin after a crappy childhood. His brother Jim is believed to be the BWL. Think you know this story? Think again. Year Four starts on 9/1/20. ",
-      username: "User 4",
-      distance: 4,
-      rating: 1.7,
-    },
-    {
-      profilePic: "https://images.ctfassets.net/usf1vwtuqyxm/6yl7KHNCnoctW5TGrOyNE1/d81e73479f15d2dfeb37dcf56c47b6b0/HP-F6-half-blood-prince-draco-malfoy-looking-concerned-web-landscape?fm=jpg&q=70&w=2560",
-      bookTitle: "Harry Potter and the Prince of Slytherin by The Sinister Man",
-      date: "December 1, 2019",
-      bookCover: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1609896187i/31201472.jpg",
-      bookDescription: "Harry Potter was Sorted into Slytherin after a crappy childhood. His brother Jim is believed to be the BWL. Think you know this story? Think again. Year Four starts on 9/1/20. NO romantic pairings prior to Fourth Year. Basically good Dumbledore and Weasleys. Limited bashing (mainly of James).",
-      username: "User 5",
-      distance: 2,
-      rating: 3.6,
-    },
-    {
-      profilePic: "https://images.ctfassets.net/usf1vwtuqyxm/6yl7KHNCnoctW5TGrOyNE1/d81e73479f15d2dfeb37dcf56c47b6b0/HP-F6-half-blood-prince-draco-malfoy-looking-concerned-web-landscape?fm=jpg&q=70&w=2560",
-      bookTitle: "Harry Potter and the Prince of Slytherin by The Sinister Man",
-      date: "December 1, 2019",
-      bookCover: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1609896187i/31201472.jpg",
-      bookDescription: "Harry Potter was Sorted into Slytherin after a crappy childhood. His brother Jim is believed to be the BWL. Think you know this story? Think again. Year Four starts on 9/1/20. NO romantic pairings prior to Fourth Year. Basically good Dumbledore and Weasleys. Limited bashing (mainly of James).",
-      username: "User 6",
-      distance: 1,
-      rating: 4.7,
-    },
-    {
-      profilePic: "https://images.ctfassets.net/usf1vwtuqyxm/6yl7KHNCnoctW5TGrOyNE1/d81e73479f15d2dfeb37dcf56c47b6b0/HP-F6-half-blood-prince-draco-malfoy-looking-concerned-web-landscape?fm=jpg&q=70&w=2560",
-      bookTitle: "Harry Potter and the Prince of Slytherin by The Sinister Man",
-      date: "December 1, 2019",
-      bookCover: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1609896187i/31201472.jpg",
-      bookDescription: "Harry Potter was Sorted into Slytherin after a crappy childhood. His brother Jim is believed to be the BWL. Think you know this story? Think again. Year Four starts on 9/1/20. ",
-      username: "User 7",
-      distance: 3,
-      rating: 5,
-    },
-    {
-      profilePic: "https://images.ctfassets.net/usf1vwtuqyxm/6yl7KHNCnoctW5TGrOyNE1/d81e73479f15d2dfeb37dcf56c47b6b0/HP-F6-half-blood-prince-draco-malfoy-looking-concerned-web-landscape?fm=jpg&q=70&w=2560",
-      bookTitle: "Harry Potter and the Prince of Slytherin by The Sinister Man",
-      date: "December 1, 2019",
-      bookCover: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1609896187i/31201472.jpg",
-      bookDescription: "Harry Potter was Sorted into Slytherin after a crappy childhood. His brother Jim is believed to be the BWL. Think you know this story? Think again. Year Four starts on 9/1/20. NO romantic pairings prior to Fourth Year. Basically good Dumbledore and Weasleys. Limited bashing (mainly of James).",
-      username: "User 8",
-      distance: 2,
-      rating: 2.3,
-    },
-    {
-      profilePic: "https://images.ctfassets.net/usf1vwtuqyxm/6yl7KHNCnoctW5TGrOyNE1/d81e73479f15d2dfeb37dcf56c47b6b0/HP-F6-half-blood-prince-draco-malfoy-looking-concerned-web-landscape?fm=jpg&q=70&w=2560",
-      bookTitle: "Harry Potter and the Prince of Slytherin by The Sinister Man",
-      date: "December 1, 2019",
-      bookCover: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1609896187i/31201472.jpg",
-      bookDescription: "Harry Potter was Sorted into Slytherin after a crappy childhood. His brother Jim is believed to be the BWL. Think you know this story? Think again. Year Four starts on 9/1/20. NO romantic pairings prior to Fourth Year. Basically good Dumbledore and Weasleys. Limited bashing (mainly of James).",
-      username: "User 9",
-      distance: 4,
-      rating: 3.5,
-    },
-    {
-      profilePic: "https://images.ctfassets.net/usf1vwtuqyxm/6yl7KHNCnoctW5TGrOyNE1/d81e73479f15d2dfeb37dcf56c47b6b0/HP-F6-half-blood-prince-draco-malfoy-looking-concerned-web-landscape?fm=jpg&q=70&w=2560",
-      bookTitle: "Harry Potter and the Prince of Slytherin by The Sinister Man",
-      date: "December 1, 2019",
-      bookCover: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1609896187i/31201472.jpg",
-      bookDescription: "Harry Potter was Sorted into Slytherin after a crappy childhood. His brother Jim is believed to be the BWL. Think you know this story? Think again. Year Four starts on 9/1/20. NO romantic pairings prior to Fourth Year. Basically good Dumbledore and Weasleys. Limited bashing (mainly of James).",
-      username: "User 10",
-      distance: 4,
-      rating: 4,
-    },
-    {
-      profilePic: "https://images.ctfassets.net/usf1vwtuqyxm/6yl7KHNCnoctW5TGrOyNE1/d81e73479f15d2dfeb37dcf56c47b6b0/HP-F6-half-blood-prince-draco-malfoy-looking-concerned-web-landscape?fm=jpg&q=70&w=2560",
-      bookTitle: "Harry Potter and the Prince of Slytherin by The Sinister Man",
-      date: "December 1, 2019",
-      bookCover: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1609896187i/31201472.jpg",
-      bookDescription: "Harry Potter was Sorted into Slytherin after a crappy childhood. His brother Jim is believed to be the BWL. Think you know this story? Think again. Year Four starts on 9/1/20. NO romantic pairings prior to Fourth Year. Basically good Dumbledore and Weasleys. Limited bashing (mainly of James).",
-      username: "User 11",
-      distance: 4,
-      rating: 2.9,
-    },
-    {
-      profilePic: "https://images.ctfassets.net/usf1vwtuqyxm/6yl7KHNCnoctW5TGrOyNE1/d81e73479f15d2dfeb37dcf56c47b6b0/HP-F6-half-blood-prince-draco-malfoy-looking-concerned-web-landscape?fm=jpg&q=70&w=2560",
-      bookTitle: "Harry Potter and the Prince of Slytherin by The Sinister Man",
-      date: "December 1, 2019",
-      bookCover: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1609896187i/31201472.jpg",
-      bookDescription: "Harry Potter was Sorted into Slytherin after a crappy childhood. His brother Jim is believed to be the BWL. Think you know this story? Think again. Year Four starts on 9/1/20. NO romantic pairings prior to Fourth Year. Basically good Dumbledore and Weasleys. Limited bashing (mainly of James).",
-      username: "User 12",
-      distance: 4,
-      rating: 0.5,
-    }
-  ]
+  useEffect(() => {
+    axios.get(`${API_URL}/user/users`)
+      .then((data) => {
+        console.log('THIS IS THE DATA', data)
+      })
+      .catch((err) => console.log('HOME SCREEN ERROR', err))
+  }, [])
 
   const nearArr = []
   for (var i = 0; i < items.length; i += 3) {
