d07d2d6ef7ed12fa3aaea9e778c286decd90113c
Aaron Yabut <aaronjiego@yahoo.com>
2022-10-06 09:24:24 -0700

    set up with the current schema

--- my-app/src/components/Home/Home.jsx
+++ my-app/src/components/Home/Home.jsx
@@ -4,32 +4,79 @@ import Item from './Item.jsx';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import { items } from './HomeTempData.jsx'
+// import { items } from './HomeTempData.jsx'
 import axios from 'axios';
-const API_URL = 'http://localhost:3001'
+const API_URL = 'http://localhost:3002'
 
 
 const Home = () => {
-  const {booksNear, setBooksNear} = useState([])
-  const {topRated, setTopRated} = useState([])
+  const [booksNear, setBooksNear] = useState([]);
+  const [usersNear, setUsersNear] = useState([]);
+  const [topRated, setTopRated] = useState([]);
+
+
+  const [users, setUsers] = useState([]);
+  const [books, setBooks] = useState([]);
+
 
   useEffect(() => {
     axios.get(`${API_URL}/user/users`)
-      .then((data) => {
-        console.log('THIS IS THE DATA', data)
+      .then((res) => {
+        setUsers(res.data)
+        console.log(res.data.length)
+
+        const booksArrayData = [];
+
+        res.data.forEach((curr, i) => {
+          axios.get(`https://www.googleapis.com/books/v1/volumes?q=${curr.books[0]}`)
+            .then((bookData) => {
+              // console.log("++", bookData.data.items[0].volumeInfo)
+              booksArrayData.push(bookData.data.items[0].volumeInfo)
+              // setBooks(booksArrayData)
+            })
+            .then(() => {
+              if (res.data.length === booksArrayData.length) {
+                setBooks(booksArrayData)
+              }
+            })
+            .catch((err) => console.log('GETTING BOOKS ERROR'))
+        })
       })
       .catch((err) => console.log('HOME SCREEN ERROR', err))
-  }, [])
+  }, []);
 
-  const nearArr = []
-  for (var i = 0; i < items.length; i += 3) {
-    nearArr.push([items[i], items[i+1], items[i+2]])
+
+  const usersData = users.slice(0, 9)
+  const booksData = books.slice(0, 9)
+
+  const recData = []
+
+  if (booksData.length !== 0) {
+    for (var p = 0; p < usersData.length; p += 3) {
+      usersData[p].books = booksData[p]
+      usersData[p+1].books = booksData[p+1]
+      usersData[p+2].books = booksData[p+2]
+      recData.push([usersData[p], usersData[p+1], usersData[p+2]])
+    }
+    // console.log('CURRENT', booksData[p])
+    console.log('ALL DATA', recData)
   }
 
-  const topArr = []
-  for (var j = 0; j < items.length; j += 3) {
-    topArr.push([items[j], items[j+1], items[j+2]])
-  }
+  console.log('USER DATA', usersData)
+  console.log('BOOKS DATA', booksData)
+
+
+
+
+  // const nearArr = []
+  // for (var i = 0; i < items.length; i += 3) {
+  //   nearArr.push([items[i], items[i+1], items[i+2]])
+  // }
+
+  // const topArr = []
+  // for (var j = 0; j < items.length; j += 3) {
+  //   topArr.push([items[j+2], items[j+1], items[j]])
+  // }
 
   return (
     <Grid
@@ -39,7 +86,7 @@ const Home = () => {
       alignItems="center"
       style={{ minHeight: '50vh' }}
     >
-      <Typography mt={5} mb={2} variant="h4">Books Near You</Typography>
+      <Typography mt={5} mb={2} variant="h4">Recommeded For You</Typography>
       <Box
         sx={{
           width: 1200,
@@ -80,8 +127,8 @@ const Home = () => {
               }
             }}
           >
-            {nearArr.length >= 1 &&
-              nearArr.map((arr, i) =>
+            {recData.length >= 1 &&
+              recData.map((arr, i) =>
               <Item
                 key={i}
                 array={arr}
@@ -131,12 +178,15 @@ const Home = () => {
               }
             }}
           >
-            {topArr.length >= 1 &&
-              topArr.map((arr, i) =>
-              <Item
-                key={i}
-                array={arr}
-              />
+            {recData.length >= 1 &&
+              recData.map((arr, i) => {
+                return (
+                  <Item
+                    key={i}
+                    array={arr}
+                  />
+                )
+              }
             )}
           </Carousel>
         </Grid>
--- my-app/src/components/Home/Item.jsx
+++ my-app/src/components/Home/Item.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Rating, Box, Card, CardHeader, Avatar, CardMedia, CardContent, Typography, CardActions, Container, Grid }
+import { Rating, Box, Card, CardHeader, Avatar, CardMedia, CardContent, Typography, CardActions, Grid }
   from '@mui/material'
-import SwapButton from "./Button";
+// import SwapButton from "./Button";
 import { styled } from '@mui/material/styles';
 import ParkIcon from '@mui/icons-material/Park';
 import ParkOutlinedIcon from '@mui/icons-material/ParkOutlined';
+import ConfirmTradeModal from '../modals/ConfirmTradeModal.jsx';
 
 const StyledRating = styled(Rating)({
   '& .MuiRating-iconFilled': {
@@ -27,42 +28,51 @@ function Item( { array } ){
               avatar={
                 <Avatar
                   alt="profile-pic"
-                  src={curr.profilePic}
+                  src={curr.profilePhoto}
                 ></Avatar>
               }
-              title={curr.bookTitle.length <= 35 ? curr.bookTitle : `${curr.bookTitle.split('').slice(0, 35).join('')}...`}
-              subheader={curr.date} // Could be date added to App or maybe a published date
+              title={curr.books.title.length <= 35 ? curr.books.title : `${curr.books.title.split('').slice(0, 35).join('')}...`}
+              subheader={curr.date} /// Could be date added to App or maybe a published date
             />
             <CardMedia
               component="img"
               height="194"
-              src={curr.bookCover}
+              src={curr.books.imageLinks.thumbnail}
               alt="Paella dish"
             />
             <CardContent>
-              {curr.bookDescription.length <= 185 ?
+              {curr.books.description.length <= 185 ?
                 <Typography variant="body2" color="text.secondary">
-                  {curr.bookDescription}
+                  {curr.books.description}
                 </Typography>
                 :
                 <Typography variant="body2" color="text.secondary">
-                  {curr.bookDescription.split('').slice(0, 185).join('')}...
+                  {curr.books.description.split('').slice(0, 185).join('')}...
                 </Typography>
               }
             </CardContent>
             <CardActions>
-              <Container>
-                <Typography>{curr.username}</Typography>
+              <Box sx={{ flexGrow: 1 }} />
+              <Box>
+                <Typography>{curr.name}</Typography>
                 <StyledRating
                   name="half-rating-read"
-                  value={curr.rating}
+                  value={(Math.random() * (5 - .5) + .5)}
                   precision={0.5}
                   icon={<ParkIcon fontSize="inherit"/>}
                   emptyIcon={<ParkOutlinedIcon fontSize="inherit"/>}
                   readOnly
                 />
-              </Container>
-              <SwapButton />
+              </Box>
+              <Box sx={{ flexGrow: 6 }} />
+              {/* <SwapButton /> */}
+              <ConfirmTradeModal
+                userName={`Keanu`}
+                userPic={`https://i.guim.co.uk/img/media/20b1ed84a85590f6ef2ef8ec4e083ededcbcb75a/445_367_3662_4578/master/3662.jpg?width=465&quality=85&dpr=1&s=none`}
+                bookName={`Diary of a Wimpy Kid`}
+                bookPic={`https://i5.walmartimages.com/asr/9efeae83-b2d6-4b16-8928-7fd44246e7dd.7fc25a4ca7f2ef37c9a307d8e5dc2291.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF`}
+              />
+              <Box sx={{ flexGrow: 1 }} />
             </CardActions>
           </Card>
           </Grid>
